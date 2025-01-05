e0e67d6b2912cb1bf696d332daf2de5d1d8cc9ad
Williams Avila <molinamolinaa54@gmail.com>
2025-01-05 14:31:12 +0100

    Documentar el main y application.properties

--- Config-Server-Weather-WhatsApp/src/main/java/org/configserverweatherwhatsapp/ConfigServerWeatherWhatsAppApplication.java
+++ Config-Server-Weather-WhatsApp/src/main/java/org/configserverweatherwhatsapp/ConfigServerWeatherWhatsAppApplication.java
@@ -2,9 +2,12 @@ package org.configserverweatherwhatsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-
+// Se debe establecer la anotación @EnableConfigServer.
+// Activa las configuraciones del ConfigServer en la aplicación de Spring Boot.
+@EnableConfigServer
 public class ConfigServerWeatherWhatsAppApplication {
 
 	public static void main(String[] args) {

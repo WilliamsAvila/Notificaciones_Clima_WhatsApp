8c3540f00db04b9698efb392a20688a58435daf8
Williams Avila <molinamolinaa54@gmail.com>
2025-01-05 14:19:57 +0100

    Crear capa de config server para la integracion de las configuraciones

---
+++ Config-Server-Weather-WhatsApp/src/main/java/org/configserverweatherwhatsapp/ConfigServerWeatherWhatsAppApplication.java
@@ -0,0 +1,14 @@
+package org.configserverweatherwhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ConfigServerWeatherWhatsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerWeatherWhatsAppApplication.class, args);
+	}
+
+}

96c986f72ee3d6f75803b4988260f1b0051d3ff3
Williams Avila <molinamolinaa54@gmail.com>
2025-01-09 20:12:10 +0100

    Creacion microservicio Twilio

---
+++ Servicio-Twilio/src/main/java/org/serviciotwilio/ServicioTwilioApplication.java
@@ -0,0 +1,13 @@
+package org.serviciotwilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioTwilioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioTwilioApplication.class, args);
+    }
+
+}

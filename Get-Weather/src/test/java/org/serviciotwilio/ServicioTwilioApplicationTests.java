4ca603f3d4b643958aef430bf34fa94581ba78e8
Williams Avila <molinamolinaa54@gmail.com>
2025-01-09 20:30:07 +0100

    Re-Nombrado del proyecto

---
+++ Get-Weather/src/test/java/org/serviciotwilio/ServicioTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serviciotwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioTwilioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

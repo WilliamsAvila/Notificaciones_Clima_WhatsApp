164aeebf1deb587cdff280b90725e46ee687b0e8
Williams Avila <molinamolinaa54@gmail.com>
2025-01-10 20:49:45 +0100

    creacion de endpoint para testear la creacion del servicio consumiento la api WeatherApi

--- Get-Weather/src/main/java/org/getweather/Controller/WeatherController.java
+++ Get-Weather/src/main/java/org/getweather/Controller/WeatherController.java
@@ -2,28 +2,35 @@ package org.getweather.Controller;
 
 import org.getweather.Service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/Weather")
 public class WeatherController {
 
+
     @Autowired
     private WeatherService weatherService;
+
+//    private final WeatherService weatherService;
+//
+//    public WeatherController(WeatherService weatherService) {
+//        this.weatherService = weatherService;
+//    }
+
 
     @GetMapping("/getWeather/{city}")
     public ResponseEntity<String> getWeather(@PathVariable String city)  {
 
         var findWeather = weatherService.getWeatherCity(city);
-        if (findWeather != null) {
-            return ResponseEntity.ok(findWeather);
+        var findMessage = weatherService.getMessage(findWeather);
+        if (findMessage != null) {
+            return ResponseEntity.ok(findMessage);
         } else {
             return ResponseEntity.notFound().build();
         }
--- Get-Weather/src/main/java/org/getweather/Service/WeatherService.java
+++ Get-Weather/src/main/java/org/getweather/Service/WeatherService.java
@@ -1,4 +1,5 @@
 package org.getweather.Service;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.URI;
@@ -36,6 +37,22 @@ public class WeatherService {
 
         }
 
+    }
+
+    public String getMessage (String responseBody) {
+
+        // Procesar la respuesta JSON
+        JSONObject respondJson = new JSONObject(responseBody);
+        JSONObject location = respondJson.getJSONObject("location");
+        String name = location.getString("name");
+        String region = location.getString("region");
+        String country = location.getString("country");
+
+        JSONObject current = respondJson.getJSONObject("current");
+        double temp_c = current.getDouble("temp_c");
+
+        return String.format("Hola en %s, %s, %s, hace %.1f ºC", name, region, country, temp_c);
+//        return "Hola en " + name + ", " + region + ", " + country + " hace " + temp_c + " ºC";
     }
 
 

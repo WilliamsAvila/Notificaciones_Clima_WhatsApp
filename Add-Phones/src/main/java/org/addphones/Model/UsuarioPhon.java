c45ba0ffcbd8a1709a54f4e63db0a7a63a93984f
Williams Avila <molinamolinaa54@gmail.com>
2025-01-15 11:48:12 +0100

    Generar clase UsuarioPhon

---
+++ Add-Phones/src/main/java/org/addphones/Model/UsuarioPhon.java
@@ -0,0 +1,24 @@
+package org.addphones.Model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class UsuarioPhon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String numeroPhone;
+    private String city;
+}

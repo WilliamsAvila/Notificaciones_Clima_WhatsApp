c0ac9d7c2e2ea7273c8d79362ab524c970112ff7
Williams Avila <molinamolinaa54@gmail.com>
2025-01-19 13:19:52 +0100

    Micro-servicio para la mensajeria de clima conectando con los 2 micro-servicio (add-phone y Get-Weather)

---
+++ Service-WhatsApp-Weather/src/main/java/org/servicewhatsappweather/ServiceWhatsAppWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.servicewhatsappweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWhatsAppWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWhatsAppWeatherApplication.class, args);
+    }
+
+}
---
+++ Service-WhatsApp-Weather/src/test/java/org/servicewhatsappweather/ServiceWhatsAppWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.servicewhatsappweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWhatsAppWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
